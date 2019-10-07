ffbbe7231ae63ea114e567485fb8833263b9425a
sofiahorenstein <51834808+sofiahorenstein@users.noreply.github.com>
2019-10-07 18:13:38 -0400

    final changes

--- app.py
+++ app.py
@@ -51,13 +51,12 @@ def precipitation():
     session = Session(engine)
 
     """Return a list of all precipitation values between 2016-08-23 and 2017-08-23"""
-    # Query all passengers
+    # Query all precipitation values
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-23").filter(Measurement.date <= "2017-08-23").all()
 
     session.close()
 
 # Create a dictionary from the row data and append to a list of all precipitation values between 2016-08-23 and 2017-08-23
-#DATE AS KEY AND PRCP AS VALUE ????
     last_year_prcp = []
     for date, prcp in results:
         prcp_dict = {}
@@ -65,7 +64,7 @@ def precipitation():
         prcp_dict["prcp"] = prcp
         last_year_prcp.append(prcp_dict)
 
-    return jsonify(prcp)
+    return jsonify(last_year_prcp)
 
 
 
@@ -75,15 +74,12 @@ def stations():
     session = Session(engine)
 
     """Return a list of all stations in the dataset"""
-    # Query all passengers
+    # Query all stations
     results = session.query(Station.station, Station.name, Station.station).all()
 
     session.close()
 
-      # Convert list of tuples into normal list
-    all_stations = list(np.ravel(results))
-
-    return jsonify(all_stations)
+    return jsonify(results)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -91,15 +87,48 @@ def tobs():
     session = Session(engine)
 
     """Return a list of Temperature Observations (tobs) for the previous year"""
-    # Query all passengers
+    # Query all temperature observations by station 
     results = session.query(Measurement.station, Measurement.date, Measurement.tobs).filter(Measurement.date >= "2016-08-23").filter(Measurement.date <= "2017-08-23").all()
     
     session.close()
+    
+    return jsonify(results)
 
       # Convert list of tuples into normal list
     last_year_tobs = list(np.ravel(results))
 
     return jsonify(last_year_tobs)
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start range"""
+    #input start date format /startdate as YYYY-MM-DD. ex: http://127.0.0.1:5000/api/v1.0/2017-08-21
+    
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= "2017-08-23").all()
+
+    session.close()
+    
+    return jsonify(results)
+
+@app.route("/api/v1.0/<start>/<end>")
+def range(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start range"""
+    #input start and end date format /startdate/enddate as YYYY-MM-DD. ex: http://127.0.0.1:5000/api/v1.0/2017-06-14/2017-08-21
+    
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
+    
+    return jsonify(results)
+
 
 
 if __name__ == '__main__':
